81fc3f3bcf8294b30710de28938601d3a00a67c7
yusufr98 <yusuf090798@gmail.com>
2019-12-12 10:05:51 -0500

    emailattach

--- src/main/java/com/example/demo/EmailService.java
+++ src/main/java/com/example/demo/EmailService.java
@@ -1,32 +1,35 @@
 package com.example.demo;
 
+import java.io.File;
 import java.util.Date;
 import java.util.Properties;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.*;
 
 public class EmailService {
-    public void sendPlainTextEmail(String subject, String message) throws AddressException,
-            MessagingException {
-        String host = "smtp.gmail.com";
-        String port = "587";
-        String mailFrom = "yusuf090798@gmail.com";
-        String password = "blastoise";
-        String mailTo = "yusufreyazuddin@gmail.com";
-        // sets SMTP server properties
-        Properties properties = new Properties();
+    private String host = "smtp.gmail.com";
+    private String port = "587";
+    private String mailFrom = "hiringmanagerdev@gmail.com";
+    private String password = "Yu$ufr98";
+    private String mailTo = "yusufreyazuddin@gmail.com";
+    // sets SMTP server properties
+    private Properties properties = new Properties();
+
+    public void setProperties(){
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", port);
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.starttls.enable", "true");
+    }
+
+    public void sendPlainTextEmail(String subject, String message) throws AddressException,
+            MessagingException {
+
+       setProperties();
 
         // creates a new session with an authenticator
         Authenticator auth = new Authenticator() {
@@ -50,23 +53,63 @@ public class EmailService {
 
         // sends the e-mail
         Transport.send(msg);
-
     }
+    public void sendEmailAttachment(String subject, String message, File file) throws AddressException,
+            MessagingException {
 
-    public static void main(String[] args) {
+        setProperties();
 
-        // outgoing message information
-        String subject = "Hello my friend";
-        String message = "Hi guy, Hope you are doing well. Duke.";
+        // creates a new session with an authenticator
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(mailFrom, password);
+            }
+        };
 
-        EmailService mailer = new EmailService();
+        Session session = Session.getInstance(properties, auth);
 
         try {
-            mailer.sendPlainTextEmail(subject, message);
-            System.out.println("Email sent.");
-        } catch (Exception ex) {
-            System.out.println("Failed to sent email.");
-            ex.printStackTrace();
+            // Create a default MimeMessage object.
+            Message message1 = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message1.setFrom(new InternetAddress(mailFrom));
+
+            // Set To: header field of the header.
+            message1.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(mailTo));
+
+            // Set Subject: header field
+            message1.setSubject(subject);
+
+            // Create the message part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Now set the actual message
+            messageBodyPart.setText(message);
+
+            // Create a multipar message
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Part two is attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = "Interview Q&A";
+            DataSource source = new FileDataSource(file);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+
+            // Send the complete message parts
+            message1.setContent(multipart);
+
+            // Send message
+            Transport.send(message1);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
